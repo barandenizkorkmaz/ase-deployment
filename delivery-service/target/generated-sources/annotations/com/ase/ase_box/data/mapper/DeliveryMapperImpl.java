c23c6d3ce11174e67af16f6f338ab157029d8a55
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2023-01-23 23:13:06 +0100

    Docker Compose init

--- delivery-service/target/generated-sources/annotations/com/ase/ase_box/data/mapper/DeliveryMapperImpl.java
+++ delivery-service/target/generated-sources/annotations/com/ase/ase_box/data/mapper/DeliveryMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-21T01:14:38+0100",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.5 (Private Build)"
+    date = "2023-01-23T22:39:01+0100",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
 )
 @Component
 public class DeliveryMapperImpl implements DeliveryMapper {
