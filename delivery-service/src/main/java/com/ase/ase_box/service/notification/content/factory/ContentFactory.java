4bff1b7fbcbea1387287b6c55b94b9eacf0d5dc1
ge28vob <deniz.korkmaz@tum.de>
2023-01-28 17:55:32 +0100

    Delete repositories

--- delivery-service/src/main/java/com/ase/ase_box/service/notification/content/factory/ContentFactory.java
+++
@@ -1,8 +0,0 @@
-package com.ase.ase_box.service.notification.content.factory;
-
-import com.ase.ase_box.data.dto.content.ContentDto;
-
-public interface ContentFactory {
-
-    ContentDto produceContent(ContentDto content);
-}
