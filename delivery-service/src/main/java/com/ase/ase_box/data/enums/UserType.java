25e37b37b9da441ff1a510fb1cc9d1548e02e240
ge28vob <deniz.korkmaz@tum.de>
2023-01-29 22:59:29 +0100

    Delete src files of auth, delivery and ui services

--- delivery-service/src/main/java/com/ase/ase_box/data/enums/UserType.java
+++
@@ -1,7 +0,0 @@
-package com.ase.ase_box.data.enums;
-
-public enum UserType {
-    CUSTOMER,
-    DELIVERER,
-    DISPATCHER
-}
--- delivery-service/src/main/java/com/ase/ase_box/security/SecurityConstants.java
+++
@@ -1,9 +0,0 @@
-package com.ase.ase_box.security;
-
-public class SecurityConstants {
-    public static final int TOKEN_EXPIRATION = 7200000; // 7200000 milliseconds = 7200 seconds = 2 hours.
-    public static final String BEARER = "Bearer "; // Authorization : "Bearer " + Token
-    public static final String AUTHORIZATION = "Authorization"; // "Authorization" : Bearer Token
-    public static final String KEY_PASSWORD = "aseproject";
-    public static final String KEY_FILE = "ase_project.keystore";
-}
