0eb63a41cf77815daa7fdbcf469f729e1d990547
ge28vob <deniz.korkmaz@tum.de>
2023-02-05 00:07:30 +0100

    Delete src files

--- frontend_v2/src/general/Logout.js
+++
@@ -1,8 +0,0 @@
-import { saveState } from "../localstorage/LocalStorage";
-
-export const logout = () =>{
-    saveState("token","");
-    saveState("userType", "");
-    saveState("login", false);
-    window.location.href = "/home";
-}
